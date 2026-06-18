c17c46b3b02710d94f30194cf14c6c97b7bb9cf2
Allison Solórzano S. <victoriasolorzano62@gmail.com>
2026-06-17 19:57:53 -0500

    170626

---
+++ modulo_3/typescriptReview/03_stringsejemplo.js
@@ -0,0 +1,11 @@
+"use strict";
+// tipos-string.ts
+const nombre = "Firulais, chowchow, cafe, AllisonS";
+const saludo = `Hola, ${nombre}`;
+const vacia = "";
+const comillas = 'También con comillas simples';
+console.log(nombre);
+console.log(saludo);
+// Métodos de string funcionan igual que en JS
+console.log(nombre.toUpperCase()); // ANA GARCÍA
+console.log(nombre.toLowerCase()); // ana garcía
---
+++ modulo_3/typescriptReview/03_stringsejemplo.ts
@@ -0,0 +1,14 @@
+// tipos-string.ts
+const nombre:    string = "Firulais, chowchow, cafe, AllisonS";
+const saludo:    string = `Hola, ${nombre}`;
+const vacia:     string = "";
+const comillas:  string = 'También con comillas simples';
+
+console.log(nombre);
+console.log(saludo);
+
+console.log(nombre.toUpperCase());     
+console.log(nombre.toLowerCase());     
+
+
+
